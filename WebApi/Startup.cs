e412659eb4a4dd4133cb6c251562688213a7e8b9
beka.brother <aka.beka94@mail.ru>
2020-10-05 20:51:41 +0600

    Add swager

--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -16,6 +16,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace WebApi
 {
@@ -48,6 +50,11 @@ namespace WebApi
 
             services.AddScoped<FlightManager>();
 
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo { Title = "Flights", Version = "v1" });
+            });
+
             services.AddControllers();
         }
 
@@ -65,6 +72,13 @@ namespace WebApi
 
             app.UseAuthentication();
             app.UseAuthorization();
+
+            app.UseSwagger(); 
+            app.UseSwaggerUI(c =>
+            {
+                c.RoutePrefix = "";
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Flights");
+            });
 
             app.UseEndpoints(endpoints =>
             {
